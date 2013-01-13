f3bf34229d8f6043fb4de1647d90bbd531537f24
will <will@twilio.com>
2013-01-13 00:22:38 -0800

    antenna configuration mode, still testing

--- WiFlyDevice.cpp
+++ WiFlyDevice.cpp
@@ -641,6 +641,14 @@ boolean WiFlyDevice::configure(byte option, unsigned long value) {
         return false;
       }
       break;
+    case ANTENNA_TYPE:
+      enterCommandMode();
+    
+      uart->print("set wlan ext_antenna ");
+      uart->println(value);
+
+      reboot();
+      break;
     default:
       return false;
       break;
